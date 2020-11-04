c9cf9307df3d5a55ed5702813247ccb12e36af01
yuto2718 <yuto2718@gmail.com>
2020-11-04 14:37:47 +0900

    test temp

--- sMain.py
+++ sMain.py
@@ -2,16 +2,16 @@ import sys
 sys.path.append("/home/pi/Documents/exp_satellite")
 
 from nineAxis import MPU9250
-#from thermal import BME280
-#from GPS import GYSFDMAXB
+from thermal import BME280
+from GPS import GYSFDMAXB
 from MONO import MONO
 from numpy import*
 
 
 import importlib
 importlib.reload(MPU9250)
-#importlib.reload(BME280)
-#importlib.reload(GYSFDMAXB)
+importlib.reload(BME280)
+importlib.reload(GYSFDMAXB)
 importlib.reload(MONO)
 
 import smbus
@@ -26,11 +26,23 @@ gpsserial = serial.Serial(GPSPORT, 115200)
 
 base = MONO.MONO(MONOPORT)
 IMU = MPU9250.MPU9250(i2c)
-#thermal = BME280.BME280(i2c)
-#gps = GYSFDMAXB.GYSFDMAXB(gpsserial)
+thermal = BME280.BME280(i2c)
+gps = GYSFDMAXB.GYSFDMAXB(gpsserial)
+
+
+pdata = 0
+for i in range(1000):
+    data = array(thermal.readData())*0.2+pacl*0.8
+    base.write(str(data[0]))
+    pdata = data
+    time.sleep(0.1)
+"""
+
+"""
 pacl = 0
 for i in range(1000):
     acl = array(IMU.getAccel())*0.2+pacl*0.8
     base.write(str(acl[2]))
     pacl = acl
     time.sleep(0.1)
+"""
