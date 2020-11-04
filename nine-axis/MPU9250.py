07b4113517d7d21a54245d26c0d3a49cea20770e
hogetarou <2020y24@gifu.kosen-ac.jp>
2020-11-04 09:55:52 +0900

    fix

--- nine-axis/MPU9250.py
+++ nine-axis/MPU9250.py
@@ -1,16 +1,17 @@
-#! /usr/bin/env python3
-#coding: utf-8
+#!/usr/bin/env python3
+# coding: utf-8
 
-# MPU9250: Slave Address - 0b1101000(gyro,accel.) & 0b1100(compass)
+# MPU9248: Slave Address - 0b1101000(gyro,accel.) & 0b1100(compass)
 
 def __word_swap(data):
     return (data & 0xFF00 >> 8) | (data & 0x00FF << 8)
+
 
 class MPU9250:
     # bus: SMBus instance
     def __init__(self, bus):
         self.bus = bus
-        self.GYRO_ADDR = 0b1101000 # AD0: L
+        self.GYRO_ADDR = 0b1101000  # AD0: L
         self.ACCEL_ADDR = 0b1101000
         self.COMPASS_ADDR = 0b1100
 
@@ -18,7 +19,7 @@ class MPU9250:
         self.ACCEL_OFFSET = 0x3B + 0x06
         self.COMPASS_OFFSET = 0x03
         self.__setup()
-        
+
     def getGyro(self):
         x = self.__readWord(self.GYRO_ADDR, self.GYRO_OFFSET)
         y = self.__readWord(self.GYRO_ADDR, self.GYRO_OFFSET + 0x02)
@@ -34,18 +35,18 @@ class MPU9250:
     def getCompass(self):
         x = self.__word_swap(self.__readWord(self.COMPASS_ADDR, self.COMPASS_OFFSET))
         y = self.__word_swap(self.__readWord(self.COMPASS_ADDR, self.COMPASS_OFFSET + 0x02))
-        z = self.__word_swap(self.__readWord(self.COMPASS_ADDR, self.COMPASS_OFFSET + 0x04))        
+        z = self.__word_swap(self.__readWord(self.COMPASS_ADDR, self.COMPASS_OFFSET + 0x04))
         return (x, y, z)
 
     def __writeReg(self, i2c_address, reg_address, data):
-        bus.write_byte_date(i2c_address, reg_address, data)
+        self.bus.write_byte_date(i2c_address, reg_address, data)
 
     def __readWord(self, i2c_addr, reg_addr):
-        msB = bus.read_byte_data(i2c_addr,reg_addr)
-        lsB = bus.read_byte_data(i2c_addr,reg_addr+1)
+        msB = self.bus.read_byte_data(i2c_addr, reg_addr)
+        lsB = self.bus.read_byte_data(i2c_addr, reg_addr + 1)
         return (msB << 8) | lsB
 
     def __setup(self):
-        self.__writeReg(self.GYRO_ADDR,0x6b,0x00)
-        self.__writeReg(self.GYRO_ADDR,0x37,0x02)
-        self.__writeReg(self.COMPASS_ADDR,0x0A,0x12)
+        self.__writeReg(self.GYRO_ADDR, 0x6b, 0x00)
+        self.__writeReg(self.GYRO_ADDR, 0x37, 0x02)
+        self.__writeReg(self.COMPASS_ADDR, 0x0A, 0x12)
