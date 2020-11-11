476908933d605605e459ff0394ec097eeb55c002
yuto2718 <yuto2718@gmail.com>
2020-11-11 10:11:11 +0900

    nya^n

--- sMain.py
+++ sMain.py
@@ -22,11 +22,12 @@ MONOPORT  = "/dev/ttyUSB0"
 GPSPORT = "/dev/ttyAMA0"
 
 i2c = smbus.SMBus(1)
-gpsserial = serial.Serial(GPSPORT, 9600)
+gpsserial = serial.Serial(GPSPORT, 57600)
 
 base = MONO.MONO(MONOPORT)
 IMU = MPU9250.MPU9250(i2c)
 thermal = BME280.BME280(i2c)
+print("IMU,thrmal init")
 gps = GYSFDMAXB.GYSFDMAXB(gpsserial)
 
 for i in range(10):
